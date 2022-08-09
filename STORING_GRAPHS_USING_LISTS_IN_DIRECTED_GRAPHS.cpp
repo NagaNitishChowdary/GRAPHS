338de2b788662989c8ae3678f8d809335b3418e3
NagaNitishChowdary <95007559+NagaNitishChowdary@users.noreply.github.com>
2022-08-09 23:40:10 +0530

    Create STORING_GRAPHS_USING_LISTS_IN_DIRECTED_GRAPHS.cpp

---
+++ STORING_GRAPHS_USING_LISTS_IN_DIRECTED_GRAPHS.cpp
@@ -0,0 +1,25 @@
+//BY USING ADJACENCY LISTS(ARRAY OF VECTORS) WE CAN MAKE WITH VERY LESS
+//SPACE COMPLEXITY (O(EDGES)) IN DIRECTED GRAPH
+
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,m;
+    cin>>n>>m;
+    vector<int>adj[n+1];
+    //n if index starts with 0
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        //adj[v].push_back(u);
+    }
+    for(int i=0;i<n+1;i++){
+        for(auto it:adj[i]){
+            cout<<it<<" ";
+        }
+        cout<<endl;
+    }
+}
